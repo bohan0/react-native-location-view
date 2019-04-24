275862ef7840cc2c7e6b78e7ae050492e6fc0182
bohan0 <bh.inbox@gmail.com>
2019-04-24 21:31:40 +1000

    add sessiontoken and restrict fields to reduce google api costs

--- src/LocationView.js
+++ src/LocationView.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React from "react";
 import {
-  View, StyleSheet, Animated, Platform, UIManager,
-  TouchableOpacity, Text, ViewPropTypes
+  View,
+  StyleSheet,
+  Animated,
+  Platform,
+  UIManager,
+  TouchableOpacity,
+  Text,
+  ViewPropTypes
 } from "react-native";
-import MapView from 'react-native-maps';
+import MapView from "react-native-maps";
 import AutoCompleteInput from "./AutoCompleteInput";
-import Events from 'react-native-simple-events';
+import Events from "react-native-simple-events";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import Entypo from "react-native-vector-icons/Entypo";
-import PropTypes from 'prop-types';
-import fetch from 'react-native-cancelable-fetch';
+import PropTypes from "prop-types";
+import fetch from "react-native-cancelable-fetch";
+import uuid from "react-native-uuid";
 
-const PLACE_DETAIL_URL = 'https://maps.googleapis.com/maps/api/place/details/json';
-const DEFAULT_DELTA = {latitudeDelta: 0.015, longitudeDelta: 0.0121};
+const PLACE_DETAIL_URL =
+  "https://maps.googleapis.com/maps/api/place/details/json";
+const DEFAULT_DELTA = { latitudeDelta: 0.015, longitudeDelta: 0.0121 };
 
 export default class LocationView extends React.Component {
   static propTypes = {
@@ -30,8 +38,8 @@ export default class LocationView extends React.Component {
   };
 
   static defaultProps = {
-    markerColor: 'black',
-    actionText: 'DONE',
+    markerColor: "black",
+    actionText: "DONE",
     onLocationSelect: () => ({}),
     debounceDuration: 300
   };
@@ -42,25 +50,37 @@ export default class LocationView extends React.Component {
     this._onTextFocus = this._onTextFocus.bind(this);
     this._onTextBlur = this._onTextBlur.bind(this);
     this._onMapRegionChange = this._onMapRegionChange.bind(this);
-    this._onMapRegionChangeComplete = this._onMapRegionChangeComplete.bind(this);
+    this._onMapRegionChangeComplete = this._onMapRegionChangeComplete.bind(
+      this
+    );
     this._onPlaceSelected = this._onPlaceSelected.bind(this);
     this._setRegion = this._setRegion.bind(this);
     this._getCurrentLocation = this._getCurrentLocation.bind(this);
-    if (Platform.OS === 'android') {
-      UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+    if (Platform.OS === "android") {
+      UIManager.setLayoutAnimationEnabledExperimental &&
+        UIManager.setLayoutAnimationEnabledExperimental(true);
     }
+    this.state = { sessionToken: uuid.v4() };
   }
 
   componentDidMount() {
-    Events.listen('InputBlur', this.constructor.displayName, this._onTextBlur);
-    Events.listen('InputFocus', this.constructor.displayName, this._onTextFocus);
-    Events.listen('PlaceSelected', this.constructor.displayName, this._onPlaceSelected);
+    Events.listen("InputBlur", this.constructor.displayName, this._onTextBlur);
+    Events.listen(
+      "InputFocus",
+      this.constructor.displayName,
+      this._onTextFocus
+    );
+    Events.listen(
+      "PlaceSelected",
+      this.constructor.displayName,
+      this._onPlaceSelected
+    );
   }
 
   componentWillUnmount() {
-    Events.rm('InputBlur', this.constructor.displayName);
-    Events.rm('InputFocus', this.constructor.displayName);
-    Events.rm('PlaceSelected', this.constructor.displayName);
+    Events.rm("InputBlur", this.constructor.displayName);
+    Events.rm("InputFocus", this.constructor.displayName);
+    Events.rm("PlaceSelected", this.constructor.displayName);
   }
 
   state = {
@@ -73,13 +93,10 @@ export default class LocationView extends React.Component {
   };
 
   _animateInput() {
-    Animated.timing(
-      this.state.inputScale,
-      {
-        toValue: this.state.inFocus ? 1.2 : 1,
-        duration: 300
-      }
-    ).start();
+    Animated.timing(this.state.inputScale, {
+      toValue: this.state.inFocus ? 1.2 : 1,
+      duration: 300
+    }).start();
   }
 
   _onMapRegionChange(region) {
@@ -104,60 +121,91 @@ export default class LocationView extends React.Component {
   }
 
   _setRegion(region, animate = true) {
-    this.state.region = {...this.state.region, ...region};
-    if (animate)
-      this._map.animateToRegion(this.state.region);
+    this.state.region = { ...this.state.region, ...region };
+    if (animate) this._map.animateToRegion(this.state.region);
   }
 
   _onPlaceSelected(placeId) {
     this._input.blur();
-    fetch(`${PLACE_DETAIL_URL}?key=${this.props.apiKey}&placeid=${placeId}`)
+    fetch(
+      `${PLACE_DETAIL_URL}?key=${
+        this.props.apiKey
+      }&placeid=${placeId}&fields=${encodeURIComponent(
+        "geometry,address_component,adr_address"
+      )}&sessiontoken=${this.state.sessionToken}`
+    )
       .then(response => response.json())
       .then(data => {
-        let region = (({lat, lng}) => ({latitude: lat, longitude: lng}))(data.result.geometry.location);
+        console.log(data);
+        let region = (({ lat, lng }) => ({ latitude: lat, longitude: lng }))(
+          data.result.geometry.location
+        );
         this._setRegion(region);
-      })
+      });
+    //changes the sessiontoken, see https://developers.google.com/places/web-service/session-tokens
+    this.setState({ sessionToken: uuid.v4() });
   }
 
   _getCurrentLocation() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      let location = (({latitude, longitude}) => ({latitude, longitude}))(position.coords);
+    navigator.geolocation.getCurrentPosition(position => {
+      let location = (({ latitude, longitude }) => ({ latitude, longitude }))(
+        position.coords
+      );
       this._setRegion(location);
     });
   }
 
   render() {
-    let {inputScale} = this.state;
+    let { inputScale } = this.state;
     return (
       <View style={styles.container}>
         <MapView
-          ref={(mapView) => this._map = mapView}
+          ref={mapView => (this._map = mapView)}
           style={styles.mapView}
           region={this.state.region}
           showsMyLocationButton={true}
           showsUserLocation={false}
-          onPress={({nativeEvent}) => this._setRegion(nativeEvent.coordinate)}
+          onPress={({ nativeEvent }) => this._setRegion(nativeEvent.coordinate)}
           onRegionChange={this._onMapRegionChange}
           onRegionChangeComplete={this._onMapRegionChangeComplete}
         />
-        <Entypo name={'location-pin'} size={30} color={this.props.markerColor} style={{backgroundColor: 'transparent'}}/>
+        <Entypo
+          name={"location-pin"}
+          size={48}
+          color={this.props.markerColor}
+          style={{ backgroundColor: "transparent" }}
+        />
         <View style={styles.fullWidthContainer}>
           <AutoCompleteInput
-            ref={input => this._input = input}
+            ref={input => (this._input = input)}
             apiKey={this.props.apiKey}
-            style={[styles.input, {transform: [{scale: inputScale}]}]}
+            style={[styles.input, { transform: [{ scale: inputScale }] }]}
             debounceDuration={this.props.debounceDuration}
+            sessionToken={this.state.sessionToken}
           />
         </View>
-        <TouchableOpacity style={[styles.currentLocBtn, {backgroundColor: this.props.markerColor}]} onPress={this._getCurrentLocation}>
-          <MaterialIcons name={'my-location'} color={'white'} size={25}/>
+        <TouchableOpacity
+          style={[
+            styles.currentLocBtn,
+            { backgroundColor: this.props.markerColor }
+          ]}
+          onPress={this._getCurrentLocation}
+        >
+          <MaterialIcons name={"my-location"} color={"white"} size={25} />
         </TouchableOpacity>
         <TouchableOpacity
           style={[styles.actionButton, this.props.actionButtonStyle]}
-          onPress={() => this.props.onLocationSelect({...this.state.region, address: this._input.getAddress()})}
+          onPress={() =>
+            this.props.onLocationSelect({
+              ...this.state.region,
+              address: this._input.getAddress()
+            })
+          }
         >
           <View>
-            <Text style={[styles.actionText, this.props.actionTextStyle]}>{this.props.actionText}</Text>
+            <Text style={[styles.actionText, this.props.actionTextStyle]}>
+              {this.props.actionText}
+            </Text>
           </View>
         </TouchableOpacity>
         {this.props.children}
@@ -169,43 +217,43 @@ export default class LocationView extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   mapView: {
     ...StyleSheet.absoluteFillObject
   },
   fullWidthContainer: {
-    position: 'absolute',
-    width: '100%',
+    position: "absolute",
+    width: "100%",
     top: 80,
-    alignItems: 'center',
+    alignItems: "center"
   },
   input: {
-    width: '80%',
+    width: "80%",
     padding: 5
   },
   currentLocBtn: {
-    backgroundColor: '#000',
+    backgroundColor: "#000",
     padding: 5,
     borderRadius: 5,
-    position: 'absolute',
+    position: "absolute",
     bottom: 70,
-    right: 10,
+    right: 10
   },
   actionButton: {
-    backgroundColor: '#000',
+    backgroundColor: "#000",
     height: 50,
-    position: 'absolute',
+    position: "absolute",
     bottom: 10,
     left: 10,
     right: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5
   },
   actionText: {
-    color: 'white',
-    fontSize: 23,
+    color: "white",
+    fontSize: 23
   }
 });
